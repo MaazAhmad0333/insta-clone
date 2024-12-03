834181ba9eb553889b4354d2b98b5b1087179356
Maaz Ahmad <ahmadmaaz0333@gmail.com>
2024-12-04 02:06:01 +0500

    changed connection configuration for server

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "123456789",
-  database: "insta_clone",
+  host: process.env.SERVERHOST,
+  user: process.env.SERVERUSER,
+  password: process.env.SERVERPASSWORD,
+  database: process.env.SERVERDATABASE,
 });
 
 module.exports = { db };
